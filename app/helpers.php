bfb3b8a65453b6f2cf24e767a11a8403116cae4e
Lennox <lennomugumira12@gmail.com>
2026-04-02 01:19:53 +0300

    created views for admins and standard users under protected,then created a helper function for admins and errors

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,14 @@
     // return Request::is($path) || Request::is($path . '/*') ? $active: '';
     return Request::is($path) || Request::is($path . '/*') ? $active: '';
  }
+
+   //Admin helper function
+ function set_active_admin($path, $active = 'active')
+{
+    return Request::is($path) ? $active : '';
+}
+  //Errors helper function
+function errors_for($attribute, $errors)
+ {
+    return $errors->first($attribute, '<p class="text-danger">:message</p>');
+ }
